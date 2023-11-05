73398d4a3f557a485d0663de8dc67060ec4b5966
Vivek <vivekkotiya007@gmail.com>
2023-11-05 23:24:53 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,32 +5,17 @@
  * @format
  */
 
-import React, { useEffect} from 'react';
+import React from 'react';
 import {
-  Alert,
   SafeAreaView,
   Text,
   View,
 } from 'react-native';
-import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
-  useEffect(() => {
-      getToken();
-  },[]);
-  const getToken = async() => {
-    const token = await messaging().getToken();
-    console.log(token);
-  }
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
-    return unsubscribe;
-  }, []);
   return (
     <SafeAreaView>
-      <View><Text>hello App</Text></View>
+      <View><Text>Welcome to App</Text></View>
     </SafeAreaView>
   );
 }
