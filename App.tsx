beeb096f0bfd5ab9c6b247e008f8c0bd648dc5ba
Vivek <vivekkotiya007@gmail.com>
2024-02-05 04:05:00 +0530

    navigation added

--- App.tsx
+++ App.tsx
@@ -11,16 +11,27 @@ import {
   Text,
   View,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './lib/src/screen/LoginScreen';
 import SignupScreen from './lib/src/screen/SignupScreen';
 
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
-    <View style={{flex:1}}>
-      {/* <LoginScreen/> */}
-      <SignupScreen/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={"LoginScreen"} component={LoginScreen} options={{title:"My Home"}}/>
+        <Stack.Screen name={"SignupScreen"} component={SignupScreen} options={{title: "Singup"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
+
+{/* <LoginScreen/> */}
+//<SignupScreen/>
+//</View>
 
 export default App;
