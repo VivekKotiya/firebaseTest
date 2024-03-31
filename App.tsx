01c1f02abf570a073aff2547b972b88dea86ac54
Vivek <vivekkotiya007@gmail.com>
2024-04-01 02:54:17 +0530

    firebase auth login sign up

--- App.tsx
+++ App.tsx
@@ -12,22 +12,8 @@ import {
   View,
 } from 'react-native';
 import Login from './lib/src/screens/loginScren/index';
-import firebase from 'firebase';
 
 const App = () => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyAvWQOS7uFWrOGJMinyaNvu4MMk3khq9Q4",
-    authDomain: "authflow-3792c.firebaseapp.com",
-    projectId: "authflow-3792c",
-    storageBucket: "authflow-3792c.appspot.com",
-    messagingSenderId: "1025392568321",
-    appId: "1:1025392568321:web:6a2607ce33a168b6a87800"
-  };
-  React.useEffect(() => {
-      // Initialize Firebase
-    const app = firebase.initializeApp(firebaseConfig);
-  
-  }, []);
   return (
     <SafeAreaView>
       <Login/>
