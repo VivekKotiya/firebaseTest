f049c3e16d1ca93c1b4c5de9fac51c5d4f481b47
Vivek <vivekkotiya007@gmail.com>
2024-04-28 21:29:01 +0530

    jest enzyme setup added

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,14 +4,17 @@
 
 import 'react-native';
 import React from 'react';
+import { Text, View } from 'react-native';
 import App from '../App';
 
 // Note: import explicitly to use the types shiped with jest.
 import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { shallow } from "enzyme";
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  //renderer.create(<App />);
+  const wrapper = shallow(<App/>);
+  expect(wrapper.find(Text).length).toBe(1);
+  expect(wrapper.find(View).length).toBe(1);
 });
+
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: 'react-native',
+  preset: "react-native",
+  coverageProvider: "v8",
+  collectCoverage: true,
+  coverageDirectory: "./coverage", // not neccessary but will create once with default name coverage
+  setupFilesAfterEnv: ["<rootDir>/__setup__/enzyme.js"],
 };
