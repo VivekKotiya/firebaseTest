6c5463ade60b168185225543db59836ee5eef979
Vivek <vivekkotiya007@gmail.com>
2024-05-22 20:55:40 +0530

    useReducer and slice store demo

--- App.tsx
+++ App.tsx
@@ -11,14 +11,21 @@ import {
   Text,
   View,
 } from 'react-native';
-import CounterView from './lib/src/component/counterView';
-
+import CounterView from './lib/src/component/useReducerComponent';
+import './lib/src/component/redux-toolkit/';
+import SongView from './lib/src/component/songView';
+import { Provider } from 'react-redux';
+import store from './lib/src/component/redux-toolkit';
+console.log("store : ", store.getState());
 const App = () => {
   return (
+    <Provider store={store}>
     <SafeAreaView>
       <View><Text>Welcome to App</Text></View>
       <CounterView/>
+      <SongView/>
     </SafeAreaView>
+    </Provider>
   );
 }
 
